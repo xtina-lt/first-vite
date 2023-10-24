610440cb366a2adbcf8616d37f49b12181a0bb6c
PLAY HARD <xtina_lt@outlook.com>
2023-10-24 17:11:36 -0400

    better screenshot

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // added for git pages deployment
+  // ads dist folder
+  base: 'https://xtina-lt.github.io/first-vite/',
   plugins: [react()],
 })
